a315a146b8d483e7d62bdf53fb054122199f1047
Mauricio <mauricio@yahoo.com>
2023-05-23 17:02:24 -0600

    library

--- src/org/bpd/PasswordSafeHelper.groovy
+++ src/org/bpd/PasswordSafeHelper.groovy
@@ -2,7 +2,7 @@ package org.bpd
 
 class PasswordSafeHelper implements Serializable {
     def steps
-    PasswordSafeHelper(this.steps = steps)
+    PasswordSafeHelper(steps) {this.steps = steps}
 
     def GetSecretSafeToken(String id){
         steps.sh "curl --insecure -X GET https://kyw3pc5rm8.execute-api.us-east-1.amazonaws.com/dev/resource"
