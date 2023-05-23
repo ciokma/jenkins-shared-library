8c90b0db32e2ac8eb904491e434c198b0d018247
Mauricio Perez <53948541+ciokma@users.noreply.github.com>
2023-05-23 10:57:31 -0600

    Update getSecret.groovy

--- vars/getSecret.groovy
+++ vars/getSecret.groovy
@@ -4,5 +4,9 @@ def call(String response = 'No Data') {
   echo "Request received, ${response}"
   String normalizedResponse = response.replace("\\", "/")
   echo "Normalized Request received, ${normalizedResponse}"
+  def jsonSlurper = new JsonSlurper()
+  def secretSafeResponse = jsonSlurper.parseText(normalizedResponse)
+  echo "${secretSafeResponse.Text}"
+
   
 }
