753d8d38b00106f3662fe0e135a6e4409d2e12a7
Mauricio Perez <53948541+ciokma@users.noreply.github.com>
2023-05-23 11:48:18 -0600

    Update getSecret.groovy

--- vars/getSecret.groovy
+++ vars/getSecret.groovy
@@ -1,6 +1,18 @@
 #!/usr/bin/env groovy
 
 import groovy.json.JsonSlurper
+//Configurando variables
+//Base: sitio de beyondtrust
+BASE = ""
+//runas: el nombre de usuario de un usuario de BeyondInsight al que se le ha concedido permiso para usar la clave de API.
+RUN_AS=""
+//Authorization=PS-Auth key=c479a66f…c9484d; runas=doe-main\johndoe; pwd=[un1qu3];
+//key: la clave de API configurada en BeyondInsight para su aplicación
+PS_AUTH_KEY=""
+//Id del secret
+SECRET_ID=""
+//Nombre del archivo cookie que se creara en <filename> después de operar
+PBPS_COOKIE_FILE= "pbpscookie.txt"
 
 def call(String response = 'No Data') {
   echo "Request received, ${response}"
@@ -9,6 +21,6 @@ def call(String response = 'No Data') {
   def jsonSlurper = new JsonSlurper()
   def secretSafeResponse = jsonSlurper.parseText(normalizedResponse)
   echo "${secretSafeResponse.Text}"
-
-  
+  sh """curl --insecure -i -c ${env.PBPS_COOKIE_FILE}  -X POST ${BASE}/Auth/SignAppin -H "Content-Type: application/json" -H "Authorization: PS-Auth key=${PS_AUTH_KEY}; runas=${RUN_AS};" -d " " """
+  return secretSafe
 }
