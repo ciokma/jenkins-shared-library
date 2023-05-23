e23aeab19eb4a46f7e2dd3e32c86c911ef7292cb
Mauricio Perez <53948541+ciokma@users.noreply.github.com>
2023-05-23 14:34:07 -0600

    Update getSecret.groovy

--- vars/getSecret.groovy
+++ vars/getSecret.groovy
@@ -1,6 +1,18 @@
 #!/usr/bin/env groovy
 
 import groovy.json.JsonSlurper
+//Configurando variables
+//Base: sitio de beyondtrust
+BASE = ""
+//runas: el nombre de usuario de un usuario de BeyondInsight al que se le ha concedido permiso para usar la clave de API.
+RUN_AS=""
+//Authorization=PS-Auth key=c479a66f…c9484d; runas=doe-main\johndoe; pwd=[un1qu3];
+//key: la clave de API configurada en BeyondInsight para su aplicación
+PS_AUTH_KEY=""
+//Id del secret
+SECRET_ID=""
+//Nombre del archivo cookie que se creara en <filename> después de operar
+PBPS_COOKIE_FILE= "pbpscookie.txt"
 
 def call(String id = 'No Data') {
   echo "Request received, ${id}"
@@ -10,8 +22,7 @@ def call(String id = 'No Data') {
   def secretSafeResponse = jsonSlurper.parseText(normalizedResponse)
   echo "${secretSafeResponse.Text}"
   
-  def response
-  response = sh(
+  def response = sh(
       script: 'curl --insecure -X GET https://hjwc4zbudb.execute-api.us-east-1.amazonaws.com/Prod/hello',
       returnStdout: true
   )
