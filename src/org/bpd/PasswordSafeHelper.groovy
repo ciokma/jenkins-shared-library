8c27772ba485beafca786a6ef9d10d73df8316b7
Mauricio <mauricio@yahoo.com>
2023-05-23 19:45:39 -0600

    consumiendo api

--- src/org/bpd/PasswordSafeHelper.groovy
+++ src/org/bpd/PasswordSafeHelper.groovy
@@ -4,11 +4,12 @@ class PasswordSafeHelper implements Serializable {
     def steps
     PasswordSafeHelper(steps) {this.steps = steps}
 
-    def GetSecretSafeToken(String id){
+    def GetSecretSafeToken(String secretid){
         steps.sh "curl --insecure -X GET https://kyw3pc5rm8.execute-api.us-east-1.amazonaws.com/dev/resource"
     }
-    def GetSecretSafeToken2(String id){
+    def GetSecretSafeToken2(String secretid){
         // GET
+        println ("GetSecretSafeToken2, token ${secretid}")
         def get = new URL("https://kyw3pc5rm8.execute-api.us-east-1.amazonaws.com/dev/resource").openConnection();
         def getRC = get.getResponseCode();
         println(getRC);
